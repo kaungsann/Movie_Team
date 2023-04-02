ff0af6d2aa7d03376a28bb286248dd10b9d81e15
Kaung Myat Hun <kaungmyathun7@gmail.com>
2023-04-02 22:31:37 +0630

    remove: app.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
-import "./App.css";
 import { BrowserRouter as Router } from "react-router-dom";
 import View from "./route";
 
