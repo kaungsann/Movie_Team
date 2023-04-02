0eeaf6a9c2eb078910929f16b153d75e1d1241f8
Kaung Myat Hun <kaungmyathun7@gmail.com>
2023-04-02 22:15:10 +0630

    adding : new component

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,6 @@ import { BrowserRouter as Router } from "react-router-dom";
 import View from "./route";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <Router>
       <View></View>
