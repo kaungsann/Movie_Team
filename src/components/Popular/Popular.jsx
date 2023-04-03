886b29e757f2a619e849df7008b02fd056bba1b8
eiswe <eiswecumwa@gmail.com>
2023-04-03 17:03:22 +0630

    create popular movies

---
+++ src/components/Popular/Popular.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Popular() {
+  return (
+    <section className="my-8">
+      <div class="md:container md:mx-auto">
+        <h3 class="mt-0 mb-2 text-3xl font-medium leading-tight text-primary">
+          Popular Movies
+        </h3>
+      </div>
+    </section>
+  );
+}
+
+export default Popular;
--- src/route.jsx
+++ src/route.jsx
@@ -3,11 +3,13 @@ import { Routes, Route } from "react-router-dom";
 
 //pages
 import MainPage from "./pages/Main";
+import PopularPage from "./components/Popular/Popular";
 
 function View() {
   return (
     <Routes>
       <Route exact path="/" element={<MainPage />}></Route>
+      <Route exact path="/popular" element={<PopularPage />}></Route>
     </Routes>
   );
 }
