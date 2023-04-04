699dc0ef11431aabc0979fb11a9b76dcc552bd80
Yel Win Thein <awin4989@gmail.com>
2023-04-04 20:48:37 +0630

    Added new feature

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import Footer from "./component/Footer";
+import FeaturedTV from "./component/FeaturedTV";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div className="App">
-      <h1 className="text-red-600">newgit</h1>
+    <div className="">
+      <FeaturedTV />
+      <Footer />
     </div>
   );
 }
