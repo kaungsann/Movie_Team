a4b2dc3a37fcfce6b3fc6eeff7fd9296aa45ff6a
eiswe <eiswecumwa@gmail.com>
2023-04-05 16:44:41 +0630

    setimage

--- src/components/Popular/Popular.jsx
+++ src/components/Popular/Popular.jsx
@@ -1,15 +1,30 @@
 import React from "react";
-
+// import Main from "./Main";
+import "./Popular.module.css";
 function Popular() {
   return (
-    <section className="my-8">
-      <div class="md:container md:mx-auto">
-        <h3 class="mt-0 mb-2 text-3xl font-medium leading-tight text-primary">
-          Popular Movies
-        </h3>
+    <div className="popular h-screen w-full bg-gradient-to-b from-black to-gray-800">
+      <div className="max-w-screen-lg mx-auto flex flex-col items-center h-full md:flex-row">
+        <div>
+          <h2 className="text-white text-3xl">
+            Popular Movies <br />
+            to Watch Now
+          </h2>
+          <p className="text-gray-400">Most watched movies by days</p>
+          <div>
+            <a href="" className="text-gray-400">VIEW ALL ></a>
+          </div>
+        </div>
+        <div className="max-w-sm rounded px-10">
+          <img src="https://source.unsplash.com/random" alt="" className="w-full"/>
+          <span className="text-gray-400">2017,Action</span>
+          <h3 className="text-white text-2xl">Movie Title</h3>
+        </div>
+        
       </div>
-    </section>
+    </div>
   );
 }
+
 
 export default Popular;
