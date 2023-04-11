3e10e3ce68a7dbf3385273f5424fd79b424c3526
Yel Win Thein <awin4989@gmail.com>
2023-04-11 06:52:59 +0630

    Added login and register

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Detai from "./pages/Detai";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route path="/detail" element={<Detai />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </BrowserRouter>
   );
