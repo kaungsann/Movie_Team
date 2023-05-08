aca8b88d5f40354f871f8a6056c1c1ab23b36429
kaung <kaungmyathun7@gmail.com>
2023-05-08 11:01:48 +0630

    change: apis

--- src/services/Constant.js
+++ src/services/Constant.js
@@ -1,13 +1,3 @@
-const request = {
-  featured: "https://fakestoreapi.com/users",
-  top9: "https://fakestoreapi.com/users",
-  moviecategory: "https://fakestoreapi.com/products?limit=5",
-  top5: "https://fakestoreapi.com/products?limit=5",
-  newTest: "https://fakestoreapi.com/users",
-};
-
-export default request;
-
 //Variables
 const VITE_APP_DOMAIN = import.meta.env.VITE_APP_DOMAIN;
 
