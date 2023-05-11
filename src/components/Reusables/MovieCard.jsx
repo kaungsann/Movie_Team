d2d6082b1931ab067dcd9b4e82534f98ebd6c9b0
Swam Htet Aung <swamhtetaung@Swams-MacBook-Air.local>
2023-05-11 19:39:31 +0630

    Fix css error in movie card in gallery

--- src/components/Reusables/MovieCard.jsx
+++ src/components/Reusables/MovieCard.jsx
@@ -7,15 +7,14 @@ const MovieCard = ({ displayStyle, notDisplay1or2, img, data }) => {
   const [overlay, setOverlay] = useState("");
   if (!notDisplay1or2) {
     return (
-      <div className="block cursor-pointer relative">
+      <div className="relative block cursor-pointer">
         <div
           onClick={() => {
             window.location.assign(`/movie-detail/${data.id}`);
           }}
           className={`group relative ${overlay}`}
           onMouseEnter={() => setOverlay("img")}
-          onMouseOut={() => setOverlay("")}
-        >
+          onMouseOut={() => setOverlay("")}>
           <img
             onClick={() => {
               window.location.assign(`/movie-detail/${data.id}`);
@@ -33,12 +32,11 @@ const MovieCard = ({ displayStyle, notDisplay1or2, img, data }) => {
               displayStyle == "display0"
                 ? "lg:w-[159px] lg:h-[238px] md:w-[162px] md:h-[243px] w-[120px] h-[180px]"
                 : "w-[193px] h-[230px]"
-            } bg-cyan-300 transition-all duration-300 top-0 opacity-0 group-[.img]:opacity-30`}
-          ></div>
+            } bg-cyan-300 transition-all duration-300 top-0 opacity-0 group-[.img]:opacity-30`}></div>
         </div>
 
         <div className="my-2">
-          <p className=" text-xs text-gray-500 mb-1">
+          <p className="mb-1 text-xs text-gray-500 ">
             {new Date(data?.release_date).toLocaleDateString()}
           </p>
           <BoldHeading customClass={"text-sm"}>{data?.title}</BoldHeading>
@@ -47,8 +45,8 @@ const MovieCard = ({ displayStyle, notDisplay1or2, img, data }) => {
     );
   } else {
     return (
-      <div className="flex items-center gap-5 group">
-        <div className="flex gap-5 relative  py-4 w-full">
+      <div className="flex items-center justify-between w-full group">
+        <div className="relative flex gap-5 py-4">
           <div className="">
             <img
               onClick={() => {
@@ -56,7 +54,7 @@ const MovieCard = ({ displayStyle, notDisplay1or2, img, data }) => {
               }}
               src={`${imgUrl}${img}`}
               alt=""
-              className={`object-cover z-10 relative ${
+              className={`object-cover z-10 relative cursor-pointer ${
                 displayStyle == "display2"
                   ? " w-[138px] h-[207px]"
                   : "w-[105px] h-[157px]"
@@ -64,36 +62,36 @@ const MovieCard = ({ displayStyle, notDisplay1or2, img, data }) => {
             />
           </div>
           <div
-            className={`absolute w-full h-full bg-gradient-to-r from-[#393e4c] via-[#181d2b] to-[#131722] z-0 top-0 opacity-0 group-hover:opacity-50`}
-          ></div>
+            className={`absolute w-full h-full bg-gradient-to-r from-[#393e4c] via-[#181d2b] to-[#131722] z-0 top-0 opacity-0 group-hover:opacity-50`}></div>
 
           <div
             className={` z-10 relative w-[75%] ${
               displayStyle !== "display2" ? "my-0" : "my-2"
-            }`}
-          >
-            <p className=" text-sm text-gray-500 ">
+            }`}>
+            <p className="text-sm text-gray-500 ">
               {new Date(data?.release_date).toLocaleDateString()}
             </p>
             <BoldHeading customClass={"text-md my-2"}>
               {data?.title}
             </BoldHeading>
-            <p className="text-gray-500 truncate text-md ">{data?.overview}</p>
+            <p className="text-gray-500 truncate text-md ">
+              {displayStyle !== "display4"
+                ? data?.overview.substr(0, 100)
+                : data?.overview.substr(0, 50) + "..."}
+            </p>
             {displayStyle !== "display4" && (
               <div
                 className={`space-x-3 ${
                   displayStyle !== "display2" ? "mt-5" : "mt-10"
-                }`}
-              >
+                }`}>
                 <button
                   onClick={() => {
                     window.location.assign(`/movie-detail/${data.id}`);
                   }}
-                  className="uppercase py-3  px-5 font-semibold text-sm bg-slate-600 rounded-md hover:border-cyan-600 hover:bg-cyan-600 transition-all duration-300 text-gray-200"
-                >
+                  className="px-5 py-3 text-sm font-semibold text-gray-200 uppercase transition-all duration-300 rounded-md bg-slate-600 hover:border-cyan-600 hover:bg-cyan-600">
                   Watch Now
                 </button>
-                <button className="uppercase py-3  px-5 font-semibold text-sm  rounded-lg hover:text-cyan-500 transition-all duration-300">
+                <button className="px-5 py-3 text-sm font-semibold uppercase transition-all duration-300 rounded-lg hover:text-cyan-500">
                   + Playlist
                 </button>
               </div>
@@ -103,18 +101,16 @@ const MovieCard = ({ displayStyle, notDisplay1or2, img, data }) => {
                 onClick={() => {
                   window.location.assign(`/movie-detail/${data.id}`);
                 }}
-                className="text-center text-gray-500 mt-5"
-              >
+                className="mt-5 text-center text-gray-500">
                 <div
                   onClick={() => {
                     window.location.assign(`/movie-detail/${data.id}`);
                   }}
                   className={`cursor-pointer ${
                     displayStyle !== "display2" && " items-center flex gap-2"
-                  }`}
-                >
+                  }`}>
                   <div className="relative group-hover:text-cyan-500">
-                    <AiFillStar className=" text-5xl" />
+                    <AiFillStar className="text-5xl " />
                     <p className=" text-sm font-semibold group-hover:text-blue-900 text-gray-200 absolute top-[50%] right-[50%] translate-x-[50%] translate-y-[-50%]">
                       {data?.vote_average}
                     </p>
@@ -130,15 +126,13 @@ const MovieCard = ({ displayStyle, notDisplay1or2, img, data }) => {
         <div
           className={`text-center text-gray-500 ${
             displayStyle == "display4" && "hidden"
-          }`}
-        >
+          }`}>
           <div
             className={`cursor-pointer flex flex-col items-center ${
               displayStyle !== "display2" && " flex-row gap-2"
-            }`}
-          >
-            <div className="relative group-hover:text-cyan-500 mx-auto">
-              <AiFillStar className=" text-5xl" />
+            }`}>
+            <div className="relative mx-auto group-hover:text-cyan-500">
+              <AiFillStar className="text-5xl " />
               <p className=" text-sm font-semibold group-hover:text-blue-900 text-gray-200 absolute top-[50%] right-[50%] translate-x-[50%] translate-y-[-50%]">
                 {data?.vote_average}
               </p>
