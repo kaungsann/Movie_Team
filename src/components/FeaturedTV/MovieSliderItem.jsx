590362cdee4824023e00aa1c5caf50e3f0b43f7d
kaung <kaungmyathun7@gmail.com>
2023-05-13 14:27:03 +0630

    update

--- src/components/FeaturedTV/MovieSliderItem.jsx
+++ src/components/FeaturedTV/MovieSliderItem.jsx
@@ -3,7 +3,12 @@ import { imgUrl } from "../../services/apiservices.js";
 
 const MovieSliderItem = ({ data }) => {
   return (
-    <div className="flex flex-col   justify-center cursor-pointer items-start">
+    <div
+      // onClick={() => {
+      //   window.location.assign(`/movie-detail/${data.id}`);
+      // }}
+      className="flex flex-col   justify-center cursor-pointer items-start"
+    >
       <a className="lg:xl:2xl:md:w-[230px] w-[100px] h-[55px] lg:xl:2xl:md:h-[130px] bg-gray-500">
         <img
           src={`${imgUrl}${data?.backdrop_path}`}
